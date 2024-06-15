c4d0f6eafc20a00dd2c7fd38395df1d3ccc869f9
barabasbernadett <dettike2001@yahoo.com>
2024-06-15 17:48:45 +0000

    title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+
+st.set_page_config(page_title='Interactive Data Explorer', page_icon='📊')
+st.title('📊 Interactive Data Explorer')
 
 df = pd.read_csv('dataset/creditcard_eligibility_dataset.csv')
 
 st.title('Credit Card Eligibility')
 st.write(df)
+
+chart = alt.Chart()
