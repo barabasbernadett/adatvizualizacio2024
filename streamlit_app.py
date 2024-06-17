4f7a10a92d7990f87365ac4f1a93b9960de009e9
barabasbernadett <dettike2001@yahoo.com>
2024-06-17 15:57:21 +0000

    new

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import altair as alt
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-st.set_page_config(page_title='Credit Card Eligibility', page_icon='📊')
-st.title('📊 Credit Card Eligibility')
+st.set_page_config(page_title='Hitelkártya jogosultságot megghatározó tényezők', page_icon='📊')
+st.title('📊 Hitelkártya jogosultságot megghatározó tényezők')
 
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
@@ -14,18 +14,23 @@ with st.expander('About this app'):
   st.markdown('**How to use the app?**')
   st.warning('To engage with the app, 1. Select genres of your interest in the drop-down selection box and then 2. Select the year duration from the slider widget. As a result, this should generate an updated editable DataFrame and line plot.')
   
-st.subheader('Which Movie Genre performs ($) best at the box office?')
+st.subheader('Adatok')
 
+st.subheader('Bevezető')
+st.info('A projekt célja a hitelkártya-jogosultsági adatbázis részletes elemzése, amely megvilágítja, hogy mely tényezők befolyásolják az egyén hitelkártya-igénylésének sikerességét. Az elemzés célja, hogy megértsük a hitelképességet befolyásoló különböző demográfiai, pénzügyi és személyes tényezőket, és ezek alapján fejlesszünk ki jobb hitelbírálati stratégiákat és célzott marketingkampányokat.')
+st.info('Az elemzéshez használt adatok egy átfogó hitelkártya-jogosultsági adatbázisból származnak, amely tartalmazza az egyes igénylők demográfiai jellemzőit (például kor, nem), pénzügyi mutatóit (például jövedelem, foglalkoztatási idő), valamint személyes adatokat (például családi állapot, lakhatási körülmények). Az adatok több mint 1000 egyénről gyűjtött információkat tartalmaznak, és változókat tartalmaznak, mint például: ID, Kor, nem , végzettség, családi állapot, lakhatási típus, hitelképesség.')     
 
 df = pd.read_csv('dataset/creditcard_eligibility_dataset.csv')
 st.write(df)
+
+st.subheader('Demográfiai változok')
 
 # Create histogram for Age Distribution
 age_hist = alt.Chart(df).mark_bar().encode(
     x=alt.X('Age:Q', bin=True, title='Age'),
     y='count()',
 ).properties(
-    title='Distribution of Age',
+    title='Egyének életkor szerinti eloszlása',
     width=600,
     height=400
 ).interactive()
@@ -33,8 +38,11 @@ age_hist = alt.Chart(df).mark_bar().encode(
 # Display Age Distribution Histogram
 st.altair_chart(age_hist, use_container_width=True)
 
+st.info('A korosztály szerinti eloszlás elemzése lehetővé teszi, hogy azonosítsuk azokat a korcsoportokat, amelyek dominálnak a hiteligénylők között és leginkább érdeklődnek a hitelkártyák irénylésére és segít abban, hogy felismerjük, mely korosztályok kevésbé képviselték magukat a hitelpiacon.')
+
+
 # Displaying gender distribution using a count bar chart
-st.header('Gender Distribution')
+st.header('Egyének nem szerinti eloszlása')
 gender_chart = alt.Chart(df).mark_bar().encode(
     alt.X('Gender:N', title='Gender'),
     alt.Y('count()', title='Number of Applicants'),
@@ -46,6 +54,8 @@ gender_chart = alt.Chart(df).mark_bar().encode(
 ).interactive()
 st.altair_chart(gender_chart, use_container_width=True)
 
+st.info('Hitelkártya jogosultsági szempontból a nemek eloszlásának szemléltetése az elemzésben segítséget nyújt abban, hogy meglássuk, mely nemek képviselői jelentkeznek gyakrabban hitelkártyákért. Ez a tudás lehetővé teszi a pénzintézetek számára, hogy jobban megértsék és célzottabban alakítsák ki a hitelkártya ajánlataikat és marketing stratégiáikat, figyelembe véve a nemek közötti pénzügyi szokások és igények különbségeit.')
+  
 # # Csoportos oszlopdiagram létrehozása
 # fig = plt.figure(figsize=(10, 8))
 # ax = fig.add_subplot(111, projection='3d')
@@ -63,6 +73,8 @@ st.altair_chart(gender_chart, use_container_width=True)
 # plt.tight_layout()
 
 # plt.show()
+
+st.subheader('A változok közötti kapcsolatok tanulmányozása')
 
 # kodreszlet 2
 import plotly.graph_objects as go
@@ -97,6 +109,9 @@ st.plotly_chart(fig)
 
 chart = alt.Chart()
 
+st.info('A családi állapot,foglalkozás és lakóhely típusa közötti összefüggéseket a hitelképességgel kapcsolatban a háromdimenziós felületi diagram szemléltet. Az eredmények alapján megállapíthatjuk, hogy például házas családos emberek esetében gyakran jobb a hitelképesség, míg egyedülállók vagy élettársakkal élők körében ez eltérhet. Emellett a hosszabb munkaviszony gyakran kedvező hatással van a hitelképességre, míg a rövidebb foglalkoztatási időszakok esetén magasabb lehet a kockázat. A lakóhely típusa (például városi vagy vidéki) szintén jelentős tényező lehet: a nagyvárosokban élők esetében esetlegesen jobb a hitelképesség, mivel magasabb jövedelmi szint és stabilitás tapasztalható.')
+
+
 # kodreszlet 3
 import plotly.express as px
 import streamlit as st
@@ -120,6 +135,8 @@ fig1 = px.scatter_3d(df,
                      color_discrete_map={'Jóváhagyott': 'green', 'Elutasított': 'red'}
                     )
 
+st.info('A vizualizáció szemlélteti, hogyan befolyásolják a családi állapot, a foglalkozás és lakóhely típusa a hitelképességet, külön kiemelve a jóváhagyott és elutasított hitelképességeket.A vizualizációk alapján számos fontos következtetést vonhatunk le a családi állapot, évek a foglalkozásban és lakóhely típusa közötti összefüggésekről a hitelképességgel kapcsolatban. Először is, látható, hogy bizonyos családi állapotok és hosszú távú munkatapasztalat jobb hitelképességet eredményezhetnek, míg másoknál ez gyengébb lehet. Ez arra utal, hogy ezek a társadalmi-gazdasági tényezők meghatározó szerepet játszanak az egyének pénzügyi stabilitásában és hitelminősítésében. Az összefüggések tanulmányozása lehetővé teszi a pénzügyi szolgáltatók számára, hogy mélyebben megértsék, milyen társadalmi-gazdasági tényezők játszanak szerepet az egyének pénzügyi stabilitásában és hitelképességében.')
+
 # 2. Vizualizáció: Jövedelem, Végzettség és Évek a foglalkozásban
 fig2 = px.scatter_3d(df, 
                      x='Total_income', 
@@ -136,9 +153,13 @@ fig2 = px.scatter_3d(df,
                      color_discrete_map={'Jóváhagyott': 'green', 'Elutasított': 'red'}
                     )
 
+
+st.info('A második vizualizáció fókuszában a jövedelem, végzettség és évek a foglalkozásban szerepelnek, és ezek kapcsán mutatja be, hogyan határozzák meg ezek a tényezők az egyén hitelképességét. Azok az egyének, akik magasabb jövedelemmel rendelkeznek és jobb végzettséggel bírnak stabilabb hitelképességgel rendelkeznek. Ez annak tudható be, hogy ezek az emberek képesek hatékonyan kezelni pénzügyi kötelezettségeiket és hosszú távú pénzügyi stabilitást biztosítani számukra.')
 # Streamlit oldalon való megjelenítés
 st.plotly_chart(fig1)
 st.plotly_chart(fig2)
+
+
 
 #kodreszlet4
 import pandas as pd
@@ -166,3 +187,116 @@ fig = px.bar(
 # Diagram megjelenítése
 st.plotly_chart(fig)
 
+#pleda5 
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+
+# Hitelképesség megjelölése
+df['Hitelképesség'] = df['Target'].replace({0: 'Elutasított', 1: 'Jóváhagyott'})
+
+# Streamlit cím
+st.title("Jövedelem típus és végzettség összefüggése hitelképesség alapján")
+
+# Grouped Bar Chart létrehozása
+fig = px.bar(
+    df,
+    x='Income_type',
+    y='ID',  # Számolja meg az egyéneket
+    color='Hitelképesség',
+    barmode='group',
+    facet_col='Education_type',
+    #labels={'ID': 'Egyének száma', 'Income_type': 'Jövedelem típus', 'Education_type': 'Végzettség'},
+    #title='Jövedelem típus és végzettség összefüggése hitelképesség alapján'
+)
+
+# Diagram megjelenítése
+st.plotly_chart(fig)
+
+#6
+df['Hitelképesség'] = df['Target'].replace({0: 'Elutasított', 1: 'Jóváhagyott'})
+
+# Streamlit cím
+st.title("Végzettség és hitelképesség összefüggése")
+
+# Grouped Bar Chart létrehozása
+fig = px.bar(
+    df,
+    x='Education_type',
+    y='ID',  # Számolja meg az egyéneket
+    color='Hitelképesség',
+    barmode='group',
+    labels={'ID': 'Egyének száma', 'Education_type': 'Végzettség'},
+    #title='Végzettség és hitelképesség összefüggése'
+)
+
+# Diagram megjelenítése
+st.plotly_chart(fig)
+
+#5.2
+# Hitelképesség megjelölése
+df['Hitelképesség'] = df['Target'].replace({0: 'Elutasított', 1: 'Jóváhagyott'})
+
+# Streamlit cím
+st.title("Jövedelem típus és hitelképesség összefüggése")
+
+# Grouped Bar Chart létrehozása
+fig = px.bar(
+    df,
+    x='Income_type',
+    y='ID',  # Számolja meg az egyéneket
+    color='Hitelképesség',
+    barmode='group',
+    labels={'ID': 'Egyének száma', 'Income_type': 'Jövedelem típus'},
+    title='Jövedelem típus és hitelképesség összefüggése'
+)
+
+# Diagram megjelenítése
+st.plotly_chart(fig)
+
+#7
+df['Hitelképesség'] = df['Target'].replace({0: 'Elutasított', 1: 'Jóváhagyott'})
+
+# Streamlit cím
+st.title("Kor és jövedelem hatása hitelképességre")
+
+# Heatmap létrehozása
+fig = px.density_heatmap(
+    df,
+    x='Age',
+    y='Total_income',
+    z='Hitelképesség',
+    color_continuous_scale='Viridis',
+    labels={'Age': 'Kor', 'Total_income': 'Jövedelem', 'Hitelképesség': 'Hitelképesség'},
+    title='Kor és jövedelem hatása hitelképességre'
+)
+
+# Diagram megjelenítése
+st.plotly_chart(fig)
+
+#kodreszlet3
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+# Hitelképesség megjelölése
+df['Hitelképesség'] = df['Target'].replace({0: 'Elutasított', 1: 'Jóváhagyott'})
+
+# Streamlit cím
+st.title("Jövedelem típus és végzettség összefüggése hitelképesség alapján")
+
+# Grouped Bar Chart létrehozása
+fig = px.bar(
+    df,
+    x='Income_type',
+    y='ID',  # Számolja meg az egyéneket
+    color='Hitelképesség',
+    barmode='group',
+    facet_col='Education_type',
+    labels={'ID': 'Egyének száma', 'Income_type': 'Jövedelem típus', 'Education_type': 'Végzettség'},
+    title='Jövedelem típus és végzettség összefüggése hitelképesség alapján'
+)
+
+# Diagram megjelenítése
+st.plotly_chart(fig)
